0942a753f119313dc934de26a0e190dbe4f8a9f2
mwirjo <mwirjosentono@byupathway.edu>
2025-07-18 17:55:06 -0300

    propets project

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -15,10 +15,15 @@ const displayProphets = (prophets) => {
     // Create elements to add to the div.cards element
     let card = document.createElement('section');
     let fullName = document.createElement('h2'); // fill in the blank
+    let birthDate = document.createElement('p');
+    let birthPlace = document.createElement('p');
     let portrait = document.createElement('img');
 
     // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    fullName.innerHTML = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    // Build the birth content
+    birthDate.innerHTML = `<strong> Date of birth:</strong> ${prophet.birthdate}`;
+    birthPlace.innerHTML = `<strong> Place of birth:</strong> ${prophet.birthplace}`;
     // Build the image portrait by setting all the relevant attributes
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
@@ -27,7 +32,10 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('height', '440');
 
     // Append the section(card) with the created elements
+   
     card.appendChild(fullName); //fill in the blank
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
     card.appendChild(portrait);
 
     cards.appendChild(card);
